835dda22faa580708da43973a24d26acadc60f55
ToniCosta96 <ToniCosta96@gmail.com>
2016-04-29 19:28:50 +0200

    CrearPlato, PerfilDeUsuario

---
+++ ProyectoIntegrado/src/interfaz/CrearPlato.java
@@ -0,0 +1,13 @@
+package interfaz;
+
+import javax.swing.JPanel;
+
+public class CrearPlato extends JPanel {
+	
+	Ventanas ventanaPrincipal;
+
+	public CrearPlato(Ventanas v) {
+		ventanaPrincipal=v;
+	}
+
+}
