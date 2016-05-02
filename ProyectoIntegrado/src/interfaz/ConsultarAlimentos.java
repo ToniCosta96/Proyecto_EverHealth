7d367aff1414174870a9f9c353b30f5ad43340da
1dam <1dam@WINMAC2.alumnado.flo>
2016-05-02 16:34:29 +0200

    consulta alimentos y consulta platos

--- ProyectoIntegrado/src/interfaz/ConsultarAlimentos.java
+++ ProyectoIntegrado/src/interfaz/ConsultarAlimentos.java
@@ -1,12 +1,80 @@
 package interfaz;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.FlowLayout;
 
 public class ConsultarAlimentos extends JPanel {
 	
 	Ventanas ventanaPrincipal;
+	private JTextField textField;
 
 	public ConsultarAlimentos(Ventanas v) {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelTituloConsultaAlimentos = new JPanel();
+		panelTituloConsultaAlimentos.setBackground(new Color(255, 255, 102));
+		add(panelTituloConsultaAlimentos, BorderLayout.NORTH);
+		
+		JLabel lblConsultaAlimentos = new JLabel("CONSULTA ALIMENTOS");
+		lblConsultaAlimentos.setForeground(new Color(255, 160, 122));
+		lblConsultaAlimentos.setFont(new Font("SimSun", Font.BOLD, 18));
+		panelTituloConsultaAlimentos.add(lblConsultaAlimentos);
+		
+		JPanel panelCentroConsultaAlimentos = new JPanel();
+		panelCentroConsultaAlimentos.setBackground(new Color(255, 255, 153));
+		add(panelCentroConsultaAlimentos, BorderLayout.CENTER);
+		panelCentroConsultaAlimentos.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setAlignmentX(0.0f);
+		panelCentroConsultaAlimentos.add(panel_2, BorderLayout.NORTH);
+		panel_2.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel panel_3 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.LEFT);
+		panel_3.setBackground(new Color(255, 255, 153));
+		panel_2.add(panel_3);
+		
+		JLabel lblBusquedaAlimentos = new JLabel("Busqueda Alimentos:");
+		lblBusquedaAlimentos.setHorizontalTextPosition(SwingConstants.LEFT);
+		lblBusquedaAlimentos.setHorizontalAlignment(SwingConstants.LEFT);
+		panel_3.add(lblBusquedaAlimentos);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setAlignmentX(0.0f);
+		panel_3.add(textField);
+		
+		JPanel panel_4 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_4.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		panel_4.setBackground(new Color(255, 255, 153));
+		panel_2.add(panel_4);
+		
+		JButton button = new JButton("Atras");
+		button.setForeground(new Color(255, 153, 51));
+		button.setBackground(new Color(255, 204, 51));
+		button.setAlignmentX(1.0f);
+		panel_4.add(button);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(new Color(255, 255, 204));
+		panelCentroConsultaAlimentos.add(panel_5, BorderLayout.CENTER);
+		panel_5.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBackground(new Color(255, 255, 204));
+		panel_5.add(scrollPane);
 		ventanaPrincipal=v;
 	}
 
--- ProyectoIntegrado/src/interfaz/ConsultarPlatos.java
+++ ProyectoIntegrado/src/interfaz/ConsultarPlatos.java
@@ -15,11 +15,22 @@ import javax.swing.JButton;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import java.awt.Component;
+import javax.swing.JScrollBar;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import net.miginfocom.swing.MigLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class ConsultarPlatos extends JPanel {
 	
 	Ventanas ventanaPrincipal;
 	private JTextField textField;
+	private JTable table;
 
 	public ConsultarPlatos(Ventanas v) {
 		setBackground(new Color(255, 255, 153));
@@ -38,43 +49,52 @@ public class ConsultarPlatos extends JPanel {
 		panelCentroConsultaPlatos.setBackground(new Color(255, 255, 153));
 		add(panelCentroConsultaPlatos, BorderLayout.CENTER);
 		
+		JPanel panelScroll = new JPanel();
+		panelScroll.setBackground(new Color(255, 255, 204));
+		
+		JPanel panelBusqueda = new JPanel();
+		panelBusqueda.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panelBusqueda.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(255, 255, 153));
+		FlowLayout flowLayout = (FlowLayout) panel_2.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panelBusqueda.add(panel_2);
+		
 		JLabel label = new JLabel("Busqueda Platos:");
+		panel_2.add(label);
+		label.setHorizontalTextPosition(SwingConstants.LEFT);
+		label.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		textField = new JTextField();
+		panel_2.add(textField);
+		textField.setAlignmentX(Component.LEFT_ALIGNMENT);
 		textField.setColumns(10);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 153));
+		FlowLayout flowLayout_1 = (FlowLayout) panel_1.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.RIGHT);
+		panelBusqueda.add(panel_1);
+		
 		JButton button = new JButton("Atras");
+		panel_1.add(button);
+		button.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		button.setForeground(new Color(255, 153, 51));
 		button.setBackground(new Color(255, 204, 51));
+		panelCentroConsultaPlatos.setLayout(new BorderLayout(0, 0));
+		panelScroll.setLayout(new GridLayout(0, 1, 0, 0));
+		panelCentroConsultaPlatos.add(panelScroll, BorderLayout.CENTER);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		GroupLayout gl_panelCentroConsultaPlatos = new GroupLayout(panelCentroConsultaPlatos);
-		gl_panelCentroConsultaPlatos.setHorizontalGroup(
-			gl_panelCentroConsultaPlatos.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panelCentroConsultaPlatos.createSequentialGroup()
-					.addGap(18)
-					.addGroup(gl_panelCentroConsultaPlatos.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 412, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_panelCentroConsultaPlatos.createSequentialGroup()
-							.addComponent(label, GroupLayout.PREFERRED_SIZE, 88, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(button, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(20, Short.MAX_VALUE))
-		);
-		gl_panelCentroConsultaPlatos.setVerticalGroup(
-			gl_panelCentroConsultaPlatos.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panelCentroConsultaPlatos.createSequentialGroup()
-					.addGroup(gl_panelCentroConsultaPlatos.createParallelGroup(Alignment.BASELINE)
-						.addComponent(button, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(label, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
-					.addContainerGap())
-		);
-		panelCentroConsultaPlatos.setLayout(gl_panelCentroConsultaPlatos);
+		scrollPane.setBackground(new Color(255, 255, 204));
+		panelScroll.add(scrollPane);
+		
+		table = new JTable();
+		table.setBackground(new Color(255, 255, 204));
+		scrollPane.setViewportView(table);
+		panelCentroConsultaPlatos.add(panelBusqueda, BorderLayout.NORTH);
 		ventanaPrincipal=v;
 	}
 }
