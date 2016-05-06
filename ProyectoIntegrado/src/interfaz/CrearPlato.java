6669e8896f666e7e7d2f3dcc75bca8476fbb349a
Alvaro <ortimorenoalvaro@gmail.com>
2016-05-06 19:50:57 +0200

    Crear plato modificada

--- ProyectoIntegrado/src/interfaz/CrearPlato.java
+++ ProyectoIntegrado/src/interfaz/CrearPlato.java
@@ -15,21 +15,19 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.JTable;
+import java.awt.Component;
 
 public class CrearPlato extends JPanel {
 	private JTextField txtCaloriasRestantes;
 	Ventanas ventanaPrincipal;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JTextField txtNombreDelPlato;
+	private JTable table;
 
 	public CrearPlato(Ventanas v) {
 		setForeground(Color.ORANGE);
 		setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		add(panel, BorderLayout.CENTER);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JPanel panelSuperior = new JPanel();
 		panelSuperior.setBackground(new Color(255, 228, 181));
@@ -53,86 +51,93 @@ public class CrearPlato extends JPanel {
 		add(Añadir_Ingrediente, BorderLayout.CENTER);
 		GridBagLayout gbl_Añadir_Ingrediente = new GridBagLayout();
 		gbl_Añadir_Ingrediente.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_Añadir_Ingrediente.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_Añadir_Ingrediente.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_Añadir_Ingrediente.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gbl_Añadir_Ingrediente.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_Añadir_Ingrediente.rowWeights = new double[]{1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		Añadir_Ingrediente.setLayout(gbl_Añadir_Ingrediente);
 		
-		JLabel Cantidad = new JLabel("Cantidad");
+		JLabel Cantidad = new JLabel("Buscador");
 		Cantidad.setForeground(new Color(255, 255, 0));
-		GridBagConstraints gbc_Cantidad = new GridBagConstraints();
-		gbc_Cantidad.insets = new Insets(0, 0, 5, 5);
-		gbc_Cantidad.anchor = GridBagConstraints.WEST;
-		gbc_Cantidad.gridx = 0;
-		gbc_Cantidad.gridy = 1;
-		Añadir_Ingrediente.add(Cantidad, gbc_Cantidad);
+		GridBagConstraints BuscadorPanel = new GridBagConstraints();
+		BuscadorPanel.insets = new Insets(0, 0, 5, 5);
+		BuscadorPanel.anchor = GridBagConstraints.EAST;
+		BuscadorPanel.gridx = 0;
+		BuscadorPanel.gridy = 1;
+		Añadir_Ingrediente.add(Cantidad, BuscadorPanel);
 		
 		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 1;
-		Añadir_Ingrediente.add(textField, gbc_textField);
+		GridBagConstraints BuscadorTxt = new GridBagConstraints();
+		BuscadorTxt.fill = GridBagConstraints.HORIZONTAL;
+		BuscadorTxt.insets = new Insets(0, 0, 5, 5);
+		BuscadorTxt.gridx = 1;
+		BuscadorTxt.gridy = 1;
+		Añadir_Ingrediente.add(textField, BuscadorTxt);
 		textField.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Ingrediente");
-		lblNewLabel.setForeground(new Color(255, 255, 0));
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 2;
-		Añadir_Ingrediente.add(lblNewLabel, gbc_lblNewLabel);
-		
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 2;
-		Añadir_Ingrediente.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
 		
 		JLabel lblAadir = new JLabel("+ A\u00F1adir");
 		lblAadir.setForeground(Color.BLUE);
-		GridBagConstraints gbc_lblAadir = new GridBagConstraints();
-		gbc_lblAadir.anchor = GridBagConstraints.WEST;
-		gbc_lblAadir.insets = new Insets(0, 0, 5, 5);
-		gbc_lblAadir.gridx = 1;
-		gbc_lblAadir.gridy = 3;
-		Añadir_Ingrediente.add(lblAadir, gbc_lblAadir);
+		GridBagConstraints AnadirTxt = new GridBagConstraints();
+		AnadirTxt.anchor = GridBagConstraints.WEST;
+		AnadirTxt.insets = new Insets(0, 0, 5, 0);
+		AnadirTxt.gridx = 2;
+		AnadirTxt.gridy = 2;
+		Añadir_Ingrediente.add(lblAadir, AnadirTxt);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 1;
-		gbc_scrollPane.gridy = 5;
-		Añadir_Ingrediente.add(scrollPane, gbc_scrollPane);
+		GridBagConstraints ScrollBuscador = new GridBagConstraints();
+		ScrollBuscador.gridheight = 8;
+		ScrollBuscador.insets = new Insets(0, 0, 5, 5);
+		ScrollBuscador.fill = GridBagConstraints.BOTH;
+		ScrollBuscador.gridx = 1;
+		ScrollBuscador.gridy = 2;
+		Añadir_Ingrediente.add(scrollPane, ScrollBuscador);
 		
 		JButton BotonAtras = new JButton("Atras");
 		GridBagConstraints gbc_BotonAtras = new GridBagConstraints();
 		gbc_BotonAtras.insets = new Insets(0, 0, 5, 5);
 		gbc_BotonAtras.gridx = 0;
-		gbc_BotonAtras.gridy = 8;
+		gbc_BotonAtras.gridy = 10;
 		BotonAtras.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				ventanaPrincipal.cambiapanel("Menu");
 			}
 		});
+		
+		JLabel lblBorrar = new JLabel("- Borrar");
+		lblBorrar.setForeground(Color.RED);
+		GridBagConstraints BorrarTxt = new GridBagConstraints();
+		BorrarTxt.anchor = GridBagConstraints.WEST;
+		BorrarTxt.insets = new Insets(0, 0, 5, 0);
+		BorrarTxt.gridx = 2;
+		BorrarTxt.gridy = 9;
+		Añadir_Ingrediente.add(lblBorrar, BorrarTxt);
 		Añadir_Ingrediente.add(BotonAtras, gbc_BotonAtras);
 		
-		JButton Guardar = new JButton("Guardar");
-		GridBagConstraints gbc_Guardar = new GridBagConstraints();
-		gbc_Guardar.insets = new Insets(0, 0, 5, 5);
-		gbc_Guardar.gridx = 1;
-		gbc_Guardar.gridy = 8;
-		Añadir_Ingrediente.add(Guardar, gbc_Guardar);
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GridBagConstraints ScrollAnadir = new GridBagConstraints();
+		ScrollAnadir.insets = new Insets(0, 0, 5, 5);
+		ScrollAnadir.fill = GridBagConstraints.BOTH;
+		ScrollAnadir.gridx = 1;
+		ScrollAnadir.gridy = 10;
+		Añadir_Ingrediente.add(scrollPane_1, ScrollAnadir);
+		
+		table = new JTable();
+		scrollPane_1.setViewportView(table);
+		
+		JButton btnGuardar = new JButton("Guardar");
+		GridBagConstraints gbc_btnGuardar = new GridBagConstraints();
+		gbc_btnGuardar.insets = new Insets(0, 0, 5, 0);
+		gbc_btnGuardar.gridx = 2;
+		gbc_btnGuardar.gridy = 10;
+		Añadir_Ingrediente.add(btnGuardar, gbc_btnGuardar);
 		ventanaPrincipal=v;
+		
+	
 		
 		
 	}
+	
+	
 
 }
