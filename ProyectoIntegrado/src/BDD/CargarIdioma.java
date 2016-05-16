1b4d94230da746e9d62f952c6d1e6aecceadae56
ToniCosta96 <ToniCosta96@gmail.com>
2016-05-16 17:36:45 +0200

    Creado archivo Config.properties y cargado idioma en CargarIdioma

--- ProyectoIntegrado/src/BDD/CargarIdioma.java
+++ ProyectoIntegrado/src/BDD/CargarIdioma.java
@@ -1,15 +1,85 @@
 package BDD;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
 import com.mysql.jdbc.Connection;
 
 public class CargarIdioma {
+	private static final String archivoConfig="./Config.properties";
 	public CargarIdioma(Conexio conexio, String arrayIdioma[]){
 		/*Se obtiene el idioma del properties y se carga el idioma de la base de datos.*/
+		String idioma=null;
 		Connection con=conexio.getConexio();
-		System.out.println("");
+		/*Cargar properties*/
+		FileInputStream input= null;
+		try{
+			Properties prop= new Properties();
+			input=new FileInputStream(archivoConfig);
+			prop.load(input);
+			prop.getProperty("idioma");
+			idioma=prop.getProperty("idioma");
+		}catch(IOException ex){
+			JOptionPane.showMessageDialog(null,
+				    ""+ex.getMessage(),
+				    "Error al guardar idioma",
+				    JOptionPane.ERROR_MESSAGE);
+		}finally{
+			if(input!=null){
+				try{
+					input.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		/*Cargar idioma de la base de datos*/
+		cargarIdiomaDeLaBDD(con,idioma,arrayIdioma);
 	}
+	
 	public CargarIdioma(Conexio conexio, String idioma, String arrayIdioma[]){
 		/*Se carga de la base de datos el idioma seleccionado y se guarda en el properties.*/
 		Connection con=conexio.getConexio();
+		/*Cargar idioma de la base de datos*/
+		cargarIdiomaDeLaBDD(con,idioma,arrayIdioma);
+		/*Escribir properties*/
+		FileOutputStream output= null;
+		try{
+			Properties prop= new Properties();
+			output=new FileOutputStream(archivoConfig);
+			prop.setProperty("idioma", idioma);
+			prop.store(output,null);
+		}catch(IOException ex){
+			JOptionPane.showMessageDialog(null,
+				    ""+ex.getMessage(),
+				    "Error al guardar idioma",
+				    JOptionPane.ERROR_MESSAGE);
+		}finally{
+			if(output!=null){
+				try{
+					output.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	private void cargarIdiomaDeLaBDD(Connection con,String idioma,String arrayIdioma[]){
+		try{
+			ResultSet rs = null;
+			Statement cmd = null;
+			cmd = (Statement) con.createStatement();
+			
+		}catch(Exception e){
+			
+		}
 	}
 }
