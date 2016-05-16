ee0044f6685083316d03fe155327d6597433c8ee
ToniCosta96 <ToniCosta96@gmail.com>
2016-05-16 16:39:15 +0200

    modificado CargarIdioma

--- ProyectoIntegrado/src/BDD/CargarIdioma.java
+++ ProyectoIntegrado/src/BDD/CargarIdioma.java
@@ -4,10 +4,12 @@ import com.mysql.jdbc.Connection;
 
 public class CargarIdioma {
 	public CargarIdioma(Conexio conexio, String arrayIdioma[]){
+		/*Se obtiene el idioma del properties y se carga el idioma de la base de datos.*/
 		Connection con=conexio.getConexio();
-		
+		System.out.println("");
 	}
 	public CargarIdioma(Conexio conexio, String idioma, String arrayIdioma[]){
+		/*Se carga de la base de datos el idioma seleccionado y se guarda en el properties.*/
 		Connection con=conexio.getConexio();
 	}
 }
