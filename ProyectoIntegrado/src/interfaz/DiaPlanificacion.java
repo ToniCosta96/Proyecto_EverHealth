6a4f3070ade7246b7809827c4e712838c3fbb344
Toni <user@user-PC>
2016-05-27 18:31:59 +0200

    Modificado SiaPlanificacion

--- ProyectoIntegrado/src/interfaz/DiaPlanificacion.java
+++ ProyectoIntegrado/src/interfaz/DiaPlanificacion.java
@@ -22,7 +22,7 @@ public class DiaPlanificacion extends JPanel {
 	 */
 	public DiaPlanificacion(String d) {
 		comboBox=new ArrayList<JComboBox<String>>();
-		textField= new JTextField[5];
+		textField= new JTextField[15];
 		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JPanel panel = new JPanel();
@@ -53,10 +53,24 @@ public class DiaPlanificacion extends JPanel {
 		JLabel lblCaloriasDesayuno = new JLabel("Calorías:");
 		panelDesayuno.add(lblCaloriasDesayuno);
 		
+		JPanel panelDesayunoCalorias = new JPanel();
+		panelDesayuno.add(panelDesayunoCalorias);
+		panelDesayunoCalorias.setLayout(new GridLayout(3, 1, 0, 0));
+		
 		textField[0] = new JTextField();
 		textField[0].setEditable(false);
-		panelDesayuno.add(textField[0]);
+		panelDesayunoCalorias.add(textField[0]);
 		textField[0].setColumns(10);
+		
+		textField[1] = new JTextField();
+		textField[1].setEditable(false);
+		panelDesayunoCalorias.add(textField[1]);
+		textField[1].setColumns(10);
+		
+		textField[2] = new JTextField();
+		textField[2].setEditable(false);
+		panelDesayunoCalorias.add(textField[2]);
+		textField[2].setColumns(10);
 		
 		JLabel lblCrearNuevoDesayuno = new JLabel("Crear nuevo desayuno");
 		panelDesayuno.add(lblCrearNuevoDesayuno);
@@ -84,10 +98,24 @@ public class DiaPlanificacion extends JPanel {
 		JLabel labelCaloriasAlmuerzo = new JLabel("Calorías:");
 		panelAlmuerzo.add(labelCaloriasAlmuerzo);
 		
-		textField[1] = new JTextField();
-		textField[1].setEditable(false);
-		textField[1].setColumns(10);
-		panelAlmuerzo.add(textField[1]);
+		JPanel panelAlmuerzoCalorias = new JPanel();
+		panelAlmuerzo.add(panelAlmuerzoCalorias);
+		panelAlmuerzoCalorias.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		textField[3] = new JTextField();
+		textField[3].setEditable(false);
+		textField[3].setColumns(10);
+		panelAlmuerzoCalorias.add(textField[3]);
+		
+		textField[4] = new JTextField();
+		textField[4].setEditable(false);
+		textField[4].setColumns(10);
+		panelAlmuerzoCalorias.add(textField[4]);
+		
+		textField[5] = new JTextField();
+		textField[5].setEditable(false);
+		panelAlmuerzoCalorias.add(textField[5]);
+		textField[5].setColumns(10);
 		
 		JLabel labelCrearAlmuerzo = new JLabel("Crear nuevo almuerzo");
 		panelAlmuerzo.add(labelCrearAlmuerzo);
@@ -115,10 +143,24 @@ public class DiaPlanificacion extends JPanel {
 		JLabel lblCalorias2 = new JLabel("Calor\u00EDas:");
 		panelComida.add(lblCalorias2);
 		
-		textField[2] = new JTextField();
-		textField[2].setEditable(false);
-		textField[2].setColumns(10);
-		panelComida.add(textField[2]);
+		JPanel panelComidaCalorias = new JPanel();
+		panelComida.add(panelComidaCalorias);
+		panelComidaCalorias.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		textField[6] = new JTextField();
+		textField[6].setEditable(false);
+		textField[6].setColumns(10);
+		panelComidaCalorias.add(textField[6]);
+		
+		textField[7] = new JTextField();
+		textField[7].setEditable(false);
+		textField[7].setColumns(10);
+		panelComidaCalorias.add(textField[7]);
+		
+		textField[8] = new JTextField();
+		textField[8].setEditable(false);
+		textField[8].setColumns(10);
+		panelComidaCalorias.add(textField[8]);
 		
 		JLabel lblCrearNuevaComida = new JLabel("Crear nueva comida");
 		panelComida.add(lblCrearNuevaComida);
@@ -151,6 +193,25 @@ public class DiaPlanificacion extends JPanel {
 		textField[3].setColumns(10);
 		panelMerienda.add(textField[3]);
 		
+		JPanel panelMeriendaCalorias = new JPanel();
+		panelMerienda.add(panelMeriendaCalorias);
+		panelMeriendaCalorias.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		textField[9] = new JTextField();
+		textField[9].setEditable(false);
+		textField[9].setColumns(10);
+		panelMeriendaCalorias.add(textField[9]);
+		
+		textField[10] = new JTextField();
+		textField[10].setEditable(false);
+		textField[10].setColumns(10);
+		panelMeriendaCalorias.add(textField[10]);
+		
+		textField[11] = new JTextField();
+		textField[11].setEditable(false);
+		textField[11].setColumns(11);
+		panelMeriendaCalorias.add(textField[10]);
+		
 		JLabel labelCrearNuevaMerienda = new JLabel("Crear nuevo desayuno");
 		panelMerienda.add(labelCrearNuevaMerienda);
 		
@@ -177,10 +238,24 @@ public class DiaPlanificacion extends JPanel {
 		JLabel label_5 = new JLabel("Calor\u00EDas:");
 		panelCena.add(label_5);
 		
-		textField[4] = new JTextField();
-		textField[4].setEditable(false);
-		textField[4].setColumns(10);
-		panelCena.add(textField[4]);
+		JPanel panelCenaCalorias = new JPanel();
+		panelCena.add(panelCenaCalorias);
+		panelCenaCalorias.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		textField[12] = new JTextField();
+		textField[12].setEditable(false);
+		textField[12].setColumns(10);
+		panelCenaCalorias.add(textField[12]);
+		
+		textField[13] = new JTextField();
+		textField[13].setEditable(false);
+		textField[13].setColumns(10);
+		panelCenaCalorias.add(textField[13]);
+		
+		textField[14] = new JTextField();
+		textField[14].setEditable(false);
+		textField[14].setColumns(10);
+		panelCenaCalorias.add(textField[14]);
 		
 		JLabel lblCrearNuevaCena = new JLabel("Crear nueva cena");
 		panelCena.add(lblCrearNuevaCena);
